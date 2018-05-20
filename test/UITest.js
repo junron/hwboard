b88d81eaed847cced8ec69d27fe0fcf1fec1b003
Jun rong <bobo999bobo@outlook.com>
2018-05-21 06:37:29 +0800

    Update UITest.js

--- test/UITest.js
+++ test/UITest.js
@@ -5,8 +5,7 @@ const server = require("../app").server
 const options = {
     headless:true,
     args: ['--no-sandbox', '--disable-setuid-sandbox','--remote-debugging-port=9222'],
-    //executablePath:"node_modules/puppeteer/.local-chromium/linux-555668/chrome-linux/chrome",
-    dumpio: true
+    //executablePath:"node_modules/puppeteer/.local-chromium/linux-555668/chrome-linux/chro
 }
 console.log(puppeteer.executablePath())
 if(process.env.CI_PROJECT_NAME=="hwboard2"){
