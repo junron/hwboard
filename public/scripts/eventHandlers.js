f5bf3271320268be5900e247e34bc9b41c8e6d3e
Jun Rong <bobo999bobo@outlook.com>
2018-06-08 08:03:22 +0800

    Adds contextmenu for non touch screens

--- public/scripts/eventHandlers.js
+++ public/scripts/eventHandlers.js
@@ -2,6 +2,10 @@ let timer
 //Required for all ppl, not just admins
 //Attach event listener to elements
 const handleStart = event => {
+  //We dont want context menu to open on mobile
+  for (const elem of document.getElementsByClassName("hwitem")){
+    elem.removeEventListener("contextmenu",rightClick,false)
+  }
   timer = setTimeout(function(){
       lastTouched = event.target
       showEditToolbar()
@@ -10,26 +14,31 @@ const handleStart = event => {
 const handleEnd = event =>{
   clearTimeout(timer)
 }
-
+const rightClick = event =>{
+  event.preventDefault()
+  menu.setPosition({top:`${event.clientY}px`,left:`${event.clientX}px`})
+  menu.open = true
+  lastTouched = event.target
+  return false
+}
 function applyHwEventHandlers(){
   for (let elem of document.getElementsByClassName("hwitem")){
-    elem.addEventListener("mousedown",handleStart)
     elem.addEventListener("touchstart",handleStart)
-    elem.addEventListener("mouseup",handleEnd)
-    elem.addEventListener("mouseleave",handleEnd)
+    elem.addEventListener("contextmenu",rightClick,false)
     elem.addEventListener("touchend",handleEnd)
   }
 }
 applyHwEventHandlers()
 
-//Hide toolbar when user clicks somewhere else
+//Hide toolbar/context menu when user clicks somewhere else
 //Requires jQuery, defer loading
 const touchedSomewhereElse = event =>{
-  if(!$(event.target).closest('#editMenu').length) {
-    if($('#editMenu').css("display")=="block") {
+  if(!($(event.target).closest('#editMenu').length + $(event.target).closest('#hw-actions').length)) {
+    if($('#editMenu').css("display")=="block" || menu.open) {
       $('#editMenu').css("display","none")
       $('#normalToolbar').css("display","initial")
       $(".mdc-toolbar").css("background-color","#6200ee")
+      menu.open = false
     }
   }
 }
