fa5a8a21b9430fce40fa38e45238ac97f232276c
Lin Yicheng <h1710085@nushigh.edu.sg>
2018-07-23 12:40:54 +0800

    Add basic calendar

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,12 +1,9 @@
 $(function() {
+
   // page is now ready, initialize the calendar...
-    const screenPadding = 50;
-    let sizeRatio = (window.innerHeight - screenPadding) / (window.innerWidth - screenPadding);
 
   $('#calendar').fullCalendar({
-      editable: false, // Don't allow editing of events
-      height: window.innerHeight - screenPadding,
-      aspectRatio: sizeRatio,
+    // put your options and callbacks here
   })
 
 });
