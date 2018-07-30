e30ebaa7635d8981e567705a87d72ef1b0fcdb80
Jro <bobo999bobo@outlook.com>
2018-07-30 21:24:03 +0800

    Increase homework load speed

--- public/scripts/fastLoadHomework.js
+++ public/scripts/fastLoadHomework.js
@@ -17,3 +17,17 @@ const sortOptions = {
 //Web worker for indexedDB
 //Use promise based messaging
 const worker = new PromiseWorker(new Worker("/scripts/worker.js"))
+
+worker.postMessage({
+  type:"get",
+}).then(data=>{
+  if(channel!=""){
+    //Only show homework for current channel
+    data = data.filter(a=>a.channel == channel)
+  }
+  if(!data.length){
+    //IndexedDB is empty, perhaps is first page load
+    return
+  }
+  reRender(data)
+})
