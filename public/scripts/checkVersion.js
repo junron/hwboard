469697d148b7fed32811837a8f351e0c8cd8e709
Jro <bobo999bobo@outlook.com>
2018-08-02 17:49:13 +0800

    Attempt at removing cache when new commit is pushed

---
+++ public/scripts/checkVersion.js
@@ -0,0 +1,14 @@
+;(async ()=>{
+  const promises = [
+    fetch("/cd/version.json")
+    .then(res=>res.json()),
+    fetch("/cd/version.json?noCache")
+    .then(res=>res.json())
+  ]
+  const result = await Promise.all(promises)
+  // New commit, refresh cache and reload page
+  if(result[1].commitSha != result[0].commitSha){
+    await caches("cache1").delete()
+    location.reload()
+  }
+})()
