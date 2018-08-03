f08117c5250b14f1e89c3280331b38e4163985d0
Lin Yicheng <h1710085@nushigh.edu.sg>
2018-08-03 23:55:09 +0800

    idk

--- public/scripts/websocketConnect.js
+++ public/scripts/websocketConnect.js
@@ -2,7 +2,7 @@ const conn = io(location.origin,{secure: true});
 //Handle websocket connection errors
 //Standard code for all my websocket apps
 if(!navigator.onLine){
-  $("#connection-status").text(`Offline`)
+  $("#connection-status").text(`Offline`);
 }
 conn.on("connect_error",function(e){
   if(navigator.onLine==undefined){
@@ -10,31 +10,40 @@ conn.on("connect_error",function(e){
     //EDIT: appears that most browsers support but meh
     //https://caniuse.com/#feat=online-status
     if(typeof Raven !="undefined"){
-      Raven.captureException(new Error("error checking connection status"))
+      Raven.captureException(new Error("error checking connection status"));
     }
-    $("#connection-status").text(`Error checking connection state`)
-    return
+    $("#connection-status").text(`Error checking connection state`);
+    return;
   }
   if(navigator.onLine){
     //Its a server problem, perhaps websocket server not started
-    $("#connection-status").text(`Error connecting to server`)
+    $("#connection-status").text(`Error connecting to server`);
     if(typeof Raven !="undefined"){
-      Raven.captureException(e)
+      Raven.captureException(e);
     }
   }else{
-    $("#connection-status").text(`Offline`)
+    $("#connection-status").text(`Offline`);
     //Load data from cache since user is offline
   }
 })
 conn.on("disconnect",function(){
-  $("#connection-status").text("Disconnected")
+  $("#connection-status").text("Disconnected");
 })
 conn.on("connect",function(){
-  $("#connection-status").text("Connected")
+  $("#connection-status").text("Connected");
+  $(document).ready(function(){
+      $('img').each(function(){
+
+          var date = new Date;
+          // add the current unix timestamp in microseconds to the the image src as a query string
+          this.src = this.src + '?' + date.getTime();
+      });
+
+  });
 })
 
 //Uncaught error that could not be handled via callback etc
 conn.on("uncaughtError",error=>{
-  Framework7App.dialog.alert(error)
-  throw new Error(error)
+  Framework7App.dialog.alert(error);
+  throw new Error(error);
 })
