762ba73bd42ec04e6f3943bea95ed5ffabd2259c
Lin Yicheng <h1710085@nushigh.edu.sg>
2018-08-07 21:48:37 +0800

    calendar add homework events
    Signed-off-by: Lin Yicheng <h1710085@nushigh.edu.sg>

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,6 +1,30 @@
+function getHomework() {
+    conn.emit("dataReq",{},(err,data)=>{
+        if(err)
+            throw err;
+        const hw = data;
+    });
+    return hw;
+}
+
+function convertHomework(arrHomework) {
+    let calendarEvents = [];
+    for (const eachHomework of arrHomework) {
+        const event ={
+            title: eachHomework.text,
+            id: eachHomework.subject,
+            start: eachHomework.dueDate,
+            allDay: true,
+        };
+        calendarEvents.appendChild(event);
+    }
+    return calendarEvents;
+}
+
 setTimeout(function() {
     const calendarPadding = 100;
     const calendarHeight = window.innerHeight - calendarPadding;
+    const homeworkEvents = convertHomework(getHomework());
 
     $('#calendar').fullCalendar({
         header: {
@@ -14,6 +38,7 @@ setTimeout(function() {
         },
         height: calendarHeight,
         editable: false,
+        events: homeworkEvents,
     });
 
 }, 100);
