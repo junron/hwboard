3f050789107c1988368c8e83aec2d901effced59
Lin Yicheng <h1710085@nushigh.edu.sg>
2018-08-07 22:48:59 +0800

    changed more formatting
    Signed-off-by: Lin Yicheng <h1710085@nushigh.edu.sg>

--- public/scripts/worker.js
+++ public/scripts/worker.js
@@ -1,16 +1,16 @@
-importScripts("/dexie/dist/dexie.min.js")
-importScripts("/promise-worker/dist/promise-worker.register.js")
-const db = new Dexie("homeworks")
+importScripts("/dexie/dist/dexie.min.js");
+importScripts("/promise-worker/dist/promise-worker.register.js");
+const db = new Dexie("homeworks");
 db.version(1).stores({
   homework: "id,subject,dueDate,isTest,text,lastEditTime,lastEditPerson"
-})
+});
 registerPromiseWorker(function (message) {
-  if(message.type=="getSingle"){
-    return db.homework.get(message.id)
-  }else if(message.type=="set"){
-    db.homework.clear()
+  if(message.type==="getSingle"){
+    return db.homework.get(message.id);
+  }else if(message.type==="set"){
+    db.homework.clear();
     return db.homework.bulkPut(message.data)
-  }else if(message.type=="get"){
-    return db.homework.toArray()
+  }else if(message.type==="get"){
+    return db.homework.toArray();
   }
 });
--- public/scripts/wsLoadHomework.js
+++ public/scripts/wsLoadHomework.js
@@ -1,7 +1,7 @@
 //request homework data from server
 //syntax conn.emit(<eventName>,[<data>],[<callback>])
 //Db is init and user is authed
-conn.on("ready",loadHomework)
+conn.on("ready",loadHomework);
 
 function loadHomework(){
   conn.emit("dataReq",channelSettings,function(err,data){
@@ -9,23 +9,23 @@ function loadHomework(){
     if(err) throw err;
     //Put data into client-side database for caching
     //But only for main page
-    if(channel==""){
+    if(channel===""){
       worker.postMessage({
         type:"set",
         data
-      })
+      });
       //Add to localstorage as a fallback
       localStorage.setItem("data",JSON.stringify(data))
     }
-    console.log("Load homework from websocket")
-    reRender(data)
+    console.log("Load homework from websocket");
+    reRender(data);
   })
 }
 //Server pushes data, re-render
 conn.on("data",({channel,data:channelData})=>{
   //Add data to client side db
-  console.log(channel,channelData)
+  console.log(channel,channelData);
   updateChannelHomework(channel,channelData).then(newData=>{
-    reRender(newData)
+    reRender(newData);
   })
-})
+});
