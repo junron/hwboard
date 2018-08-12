e2817f010737da8f4850d95eced82c15fcd9b513
Jro <bobo999bobo@outlook.com>
2018-08-12 14:53:45 +0800

    Use websocket transports only
    Prevent issues with round robin load balancing

--- public/scripts/websocketConnect.js
+++ public/scripts/websocketConnect.js
@@ -1,6 +1,7 @@
 const conn = io(location.origin,{
   secure: true,
   reconnectionDelay:100,
+  transports:["websocket"]
 });
 //Handle websocket connection errors
 //Standard code for all my websocket apps
