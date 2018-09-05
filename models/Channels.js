d86be318da749975946db869d341d634722bee61
Jro <bobo999bobo@outlook.com>
2018-09-05 10:55:12 +0800

    Use UUIDs for channel ids

--- models/Channels.js
+++ models/Channels.js
@@ -1,32 +1,31 @@
 module.exports = (sequelize, Sequelize) => {
-    return sequelize.define('channels', {
-      id: {
-          type: Sequelize.INTEGER,
-          autoIncrement: true,
-          allowNull: false,
-          primaryKey: true
-      },
-      name: {
-          type: Sequelize.STRING
-      },
-      subjects: {
-          type: Sequelize.ARRAY(Sequelize.STRING)
-      },
-      timetable: {
-          type: Sequelize.JSON
-      },
-      roots: {
-          type: Sequelize.ARRAY(Sequelize.STRING)
-      },
-      admins: {
-          type: Sequelize.ARRAY(Sequelize.STRING)
-      },
-      members: {
-          type: Sequelize.ARRAY(Sequelize.STRING)
-      },
+  return sequelize.define('channels', {
+    id :{
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING
+    },
+    subjects: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    timetable: {
+        type: Sequelize.JSON
+    },
+    roots: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    admins: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    members: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+    },
   }, {
-      timestamps: true,
-      createdAt: false,
-      updatedAt: 'lastEditTime'
+    timestamps: true,
+    createdAt: false,
+    updatedAt: 'lastEditTime'
   })
 };
