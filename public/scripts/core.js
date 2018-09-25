41cea3bc77a626696295fd277222d4d41903f17e
Jro <bobo999bobo@outlook.com>
2018-09-25 19:56:35 +0800

    Use centralized interface for getting data

---
+++ public/scripts/core.js
@@ -0,0 +1,104 @@
+const hwboard = (()=>{
+  //Web worker for indexedDB
+  //Use promise based messaging
+  worker = new PromiseWorker(new Worker("/scripts/worker.js"))
+  /**
+   * Gets homework from websocket or cache
+   * @param {Boolean} removeExpired Whether to remove expired homework
+   */
+  async function getHomework(removeExpired=true){
+    if(typeof worker==="undefined"){
+      worker = new PromiseWorker(new Worker("/scripts/worker.js"))
+      console.log("Worker was not initalized.")
+      console.log("Worker is now a(n)",typeof worker)
+    }
+    const promises = []
+    if(typeof conn !== "undefined"){
+      promises.push(new Promise((resolve,reject)=>{
+        const settings = Object.assign({},channelSettings)
+        settings.removeExpired = removeExpired
+        conn.emit("dataReq",settings,function(err,data){
+          //Always check if error occurred
+          if(err) throw err;
+          //Put data into client-side database for caching
+          //But only for main page
+          if(channel==""){
+            worker.postMessage({
+              type:"set",
+              data
+            })
+            //Add to localstorage as a fallback
+            localStorage.setItem("data",JSON.stringify(data))
+          }
+          console.log("Load homework from websocket")
+          resolve(data)
+        })
+      }))
+    }
+    promises.push(worker.postMessage({
+      type:"get",
+    }).then(data=>{
+      console.log("Load homework from Indexeddb")
+      if(channel!=""){
+        //Only show homework for current channel
+        data = data.filter(a=>a.channel == channel)
+      }
+      if(!data.length){
+        //IndexedDB is empty, perhaps is first page load
+        return []
+      }
+      return data
+    }))
+    return {
+      quickest:await Promise.race(promises),
+      promises
+    }
+  }
+  /**
+   * Gets channel data from websocket or cache, whichever is fastest
+   */
+  async function getChannelData(){
+    if(typeof worker==="undefined"){
+      worker = new PromiseWorker(new Worker("/scripts/worker.js"))
+      console.log("Worker was not initalized.")
+      console.log("Worker is now a(n)",typeof worker)
+    }
+    const promises = []
+    promises.push(worker.postMessage({
+      type:"getChannels",
+    }).then(data=>{
+      console.log("Load channels from Indexeddb")
+      if(!data.length){
+        //IndexedDB is empty, perhaps is first page load
+        return false
+      }
+      return data
+    }))
+    if(typeof conn!="undefined"&&conn.connected){
+      //Connected before page load
+      promises.push(new Promise((resolve,reject)=>{
+        conn.emit("channelDataReq",{},function(err,data){
+          //Always check if error occurred
+          if(err) throw err;
+          //Put channel data into client-side database for caching and offline access
+          worker.postMessage({
+            type:"setChannels",
+            data
+          })
+          //Add to localstorage as a fallback
+          localStorage.setItem("channelData",JSON.stringify(data))
+          console.log("Load channels from websocket")
+          resolve(data)
+        })
+      }))
+    }
+    return {
+      quickest:await Promise.race(promises),
+      promises
+    }
+  }
+  return {
+    getHomework,
+    getChannelData
+  }
+})()
--- public/scripts/loadHomework.js
+++ public/scripts/loadHomework.js
@@ -22,87 +22,9 @@ channelSettings = {
   removeExpired:true
 }
 
-//Load cached data before websocket connects
-//Allows for faster loading of updated data
-async function loadHomeworkFromCache(){
-  const promises = []
-
-  //Get homework data
-  promises.push(worker.postMessage({
-    type:"get",
-  }).then(data=>{
-    console.log("Load homework from Indexeddb")
-    if(channel!=""){
-      //Only show homework for current channel
-      data = data.filter(a=>a.channel == channel)
-    }
-    if(!data.length){
-      //IndexedDB is empty, perhaps is first page load
-      return []
-    }
-    return data
-  }))
-  
-  //Get channel data
-  promises.push(worker.postMessage({
-    type:"getChannels",
-  }).then(data=>{
-    console.log("Load channels from Indexeddb")
-    if(!data.length){
-      //IndexedDB is empty, perhaps is first page load
-      return false
-    }
-    setSubjectVariables(data)
-    return true
-  }))
-  const [data,channelResult] = await Promise.all(promises)
-  if(data.length&&channelResult){
-    reRender(data)
-  }
+async function loadHomework(){
+  const [homeworkData,channelData] = (await Promise.all([hwboard.getHomework(),hwboard.getChannelData()])).map(res=>res.quickest)
+  setSubjectVariables(channelData);
+  reRender(homeworkData)
 }
-
-loadHomeworkFromCache()
-
-if(typeof conn!="undefined"){
-  //Connected before page load
-  conn.emit("isReady",null,(res)=>{
-    if(res){
-      loadHomework()
-    }
-  })
-}
-
-async function loadChannelData(){
-  const promises = []
-  promises.push(worker.postMessage({
-    type:"getChannels",
-  }).then(data=>{
-    console.log("Load channels from Indexeddb")
-    if(!data.length){
-      //IndexedDB is empty, perhaps is first page load
-      return false
-    }
-    setSubjectVariables(data)
-    return true
-  }))
-  if(typeof conn!="undefined"&&conn.connected){
-    //Connected before page load
-    promises.push(new Promise((resolve,reject)=>{
-      conn.emit("channelDataReq",{},function(err,data){
-        //Always check if error occurred
-        if(err) throw err;
-        //Put channel data into client-side database for caching and offline access
-        worker.postMessage({
-          type:"setChannels",
-          data
-        })
-        //Add to localstorage as a fallback
-        localStorage.setItem("channelData",JSON.stringify(data))
-        setSubjectVariables(data)
-        console.log("Load channels from websocket")
-        resolve(true)
-      })
-    }))
-  }
-  return Promise.race(promises)
-}
+loadHomework()
