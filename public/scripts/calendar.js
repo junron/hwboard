48a3e8b6f63cc17793a4a007c38c31e836461e9a
Jro <bobo999bobo@outlook.com>
2018-09-25 16:24:34 +0800

    Improve contrast of calendar text

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,16 +1,25 @@
 colors = ["#ff5252", "#ff4081", "#e040fb", "#7c4dff", "#536dfe", "#448aff", "#40c4ff", "#18ffff", "#64ffda", "#69f0ae", "#b2ff59", "#eeff41", "#ffff00", "#ffd740", "#ffab40", "#ff6e40"];
 
+function picktextColor(bgColor, lightColor, darkColor) {
+    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    return (Math.round(((r * 0.299) + (g * 0.587) + (b * 0.114))) >= 186) ?
+      darkColor : lightColor;
+  }
 
 function convertHomework(arrHomework) {
     let calendarEvents = [];
     for (const eachHomework of arrHomework) {
-        const eventColor = subjectColors[0][subjectColors[1].findIndex(function (findSubject) {return findSubject === eachHomework.subject})];
+        const eventColor = subjectColors[0][subjectColors[1].findIndex(function (findSubject) {return findSubject === eachHomework.subject})] || "#3B3EAC"
         const event ={
             title: eachHomework.text,
             id: eachHomework.subject,
             start: eachHomework.dueDate.slice(0, 10),
             allDay: true,
-            color: eventColor
+            color: eventColor,
+            textColor:picktextColor(eventColor,"#ffffff","#000000")
         };
         calendarEvents.push(event);
     }
