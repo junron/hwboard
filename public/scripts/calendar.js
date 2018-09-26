3804f8d2728a3689ba794926ea602b832c2684ea
Jro <bobo999bobo@outlook.com>
2018-09-26 20:41:49 +0800

    Remove some unnecssary files

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -5,7 +5,7 @@ function picktextColor(bgColor, lightColor, darkColor) {
     var r = parseInt(color.substring(0, 2), 16); // hexToR
     var g = parseInt(color.substring(2, 4), 16); // hexToG
     var b = parseInt(color.substring(4, 6), 16); // hexToB
-    return (Math.round(((r * 0.299) + (g * 0.587) + (b * 0.114))) >= 186) ?
+    return (Math.round(((r * 0.299) + (g * 0.587) + (b * 0.114))) >= 180) ?
       darkColor : lightColor;
   }
 
