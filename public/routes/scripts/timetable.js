d91bccfe3faa7ebf64211588fb943092ee42abd0
Jro <bobo999bobo@outlook.com>
2018-10-21 15:46:18 +0800

    Use new color gradient

--- public/routes/scripts/timetable.js
+++ public/routes/scripts/timetable.js
@@ -1,13 +1,4 @@
 async function renderTimetable(){
-  function pickTextColor(bgColor, lightColor, darkColor) {
-    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
-    var r = parseInt(color.substring(0, 2), 16); // hexToR
-    var g = parseInt(color.substring(2, 4), 16); // hexToG
-    var b = parseInt(color.substring(4, 6), 16); // hexToB
-    return (Math.round(((r * 0.299) + (g * 0.587) + (b * 0.114))) >= 180) ?
-      darkColor : lightColor;
-  }
-  const colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC']
   if(!Object.keys(timetable).length){
     await loadHomework()
   }
@@ -22,9 +13,10 @@ async function renderTimetable(){
   }
   const modifiedTimetable = Object.assign(assemblyAndCCA,timetable)
   const events = []
+  const colors = tinygradient(['#00e5bc', '#8ed200', '#c42700']).hsv(Object.keys(modifiedTimetable).length)
   for(let subject in modifiedTimetable){
     const lessons = modifiedTimetable[subject]
-    const color = colors[Object.keys(modifiedTimetable).indexOf(subject)] || "#3B3EAC"
+    const color = colors[Object.keys(modifiedTimetable).indexOf(subject)].toHexString()
     for (const dayName in lessons){
       const day = lessons[dayName][0]
       const eventStart = Sugar.Date.create(`${dayName} ${Math.floor(day[0]/100)}:${(day[0] % 100).toString().padStart(2,"0")}`)
@@ -40,13 +32,15 @@ async function renderTimetable(){
           }
         }
       }
+      let textColor = tinycolor.mostReadable(color,["#fff","#000"]).toHexString()
+      textColor = (textColor==="#000000" && tinycolor.readability(textColor,color)<5) ? "#fff" : textColor
       events.push({
         title:subject,
         start:eventStart,
         end:eventEnd,
         allDay:false,
         color,
-        textColor:pickTextColor(color,"white","black")+";font-size:1.2em;"
+        textColor:textColor+";font-size:1.2em;"
       })
     }
   }
