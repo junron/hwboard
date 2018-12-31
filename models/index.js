4cc61e9782f6022d31423389d1c0475f4d6d1b3c
Jun Rong <bobo999bobo@outlook.com>
2018-12-31 17:41:49 +0800

    Add support for postgres/cockroachdb

--- models/index.js
+++ models/index.js
@@ -1,59 +1,64 @@
-const Sequelize = require('sequelize-cockroachdb');
-
-//Make sequelize arrays work for cockroachDB
-//https://github.com/cockroachdb/sequelize-cockroachdb/issues/26
-Sequelize.ARRAY.prototype._stringify = function _stringify(values, options) {
-  let str = 'ARRAY[' + values.map(value => {
-    if (this.type && this.type.stringify) {
-      value = this.type.stringify(value, options);
-
-      if (this.type.escape === false) {
-        return value;
-      }
-    }
-    return options.escape(value);
-  }, this).join(',') + ']';
-
-  if (this.type) {
-    const Utils = require('sequelize/lib/utils');
-    let castKey = this.toSql();
-
-    if (this.type instanceof Sequelize.ENUM) {
-      castKey = Utils.addTicks(
-        Utils.generateEnumName(options.field.Model.getTableName(), options.field.fieldName),
-        '"'
-      ) + '[]';
-    }
-    //3 colons instead of 2
-    str += ':::' + castKey;
-  }
-
-  return str;
-};
-
+// Load variables
 //Same as docker env variables 
 //or use a config file
 const {
-  POSTGRES_PASSWORD:dbPassword,
-  POSTGRES_USER:dbUser,
-  POSTGRES_DB:dbName="hwboard",
+  DB_PASSWORD:dbPassword,
+  DB_USER:dbUser,
+  DB_NAME:dbName="hwboard",
+  USE_POSTGRES:psql,
   SEQUELIZE_LOGGING:logging,
-  COCKROACH_DB_PORT:cockroachDBPort,
+  DB_PORT:dbPort,
   COCKROACH_DB_SECURE:cockroachSecure
 } = require("../loadConfig")
 
+const Sequelize = psql ? require("sequelize") : require('sequelize-cockroachdb');
 
-let POSTGRES_HOST = "localhost"
+if(!psql){
+  //Make sequelize arrays work for cockroachDB
+  //https://github.com/cockroachdb/sequelize-cockroachdb/issues/26
+  Sequelize.ARRAY.prototype._stringify = function _stringify(values, options) {
+    let str = 'ARRAY[' + values.map(value => {
+      if (this.type && this.type.stringify) {
+        value = this.type.stringify(value, options);
+
+        if (this.type.escape === false) {
+          return value;
+        }
+      }
+      return options.escape(value);
+    }, this).join(',') + ']';
+
+    if (this.type) {
+      const Utils = require('sequelize/lib/utils');
+      let castKey = this.toSql();
+
+      if (this.type instanceof Sequelize.ENUM) {
+        castKey = Utils.addTicks(
+          Utils.generateEnumName(options.field.Model.getTableName(), options.field.fieldName),
+          '"'
+        ) + '[]';
+      }
+      //3 colons instead of 2
+      str += ':::' + castKey;
+    }
+
+    return str;
+  };
+}
+
+
+
+let DB_HOST = "localhost"
 //In docker, cockroachDB is on `cockroachdb` and not `localhost`
 //Cos db container is called cockroachdb
 //https://forums.docker.com/t/cant-get-postgres-to-work/29580/4
 if(process.env.CI_PROJECT_NAME=="hwboard2" || process.env.IS_DOCKER=="true"){
-  POSTGRES_HOST = "cockroachdb"
+  DB_HOST = psql ? "postgres" : "cockroachdb"
 }
 
 const config = {
-  host:POSTGRES_HOST,
-  port:cockroachDBPort,
+  host:DB_HOST,
+  port:dbPort,
   dialect: "postgres",
   operatorsAliases: Sequelize.Op
 }
@@ -61,7 +66,7 @@ console.log({logging})
 if(logging===false){
   config.logging = false
 }
-if(cockroachSecure && process.env.CI_PROJECT_NAME!=="hwboard2"){
+if(!psql && cockroachSecure && process.env.CI_PROJECT_NAME!=="hwboard2"){
   const fs = require("fs")
   config.dialectOptions = {
     ssl: {
