01324af17b77a7b905ae052a12892e081d67adf2
Lin Yicheng <h1710085@nushigh.edu.sg>
2019-02-01 10:59:55 +0800

    added channel delete and channel edit functions
    Signed-off-by: Lin Yicheng <h1710085@nushigh.edu.sg>

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,7 @@ const {sequelize} = require("../models");
 
 const admin = require("./admin");
 const homework = require("./homework");
+const channel = require("./channel");
 
 //Generate tables
 async function init(){
@@ -17,23 +18,25 @@ async function init(){
 
 
 module.exports={
+  init,
   sequelize,
   getHomework:homework.getHomework,
   addHomework:homework.addHomework,
   editHomework:homework.editHomework,
   deleteHomework:homework.deleteHomework,
-  init,
-  getUserChannels:admin.getUserChannels,
-  getUserChannel:admin.getUserChannel,
   getHomeworkAll:homework.getHomeworkAll,
-  addMember:admin.addMember,
-  removeMember:admin.removeMember,
-  addSubject:admin.addSubject,
   getNumTables: homework.getNumTables,
   whenHomeworkExpires:homework.whenHomeworkExpires,
   getNumHomework:homework.getNumHomework,
   removeSubject:admin.removeSubject,
+  getUserChannels:admin.getUserChannels,
+  getUserChannel:admin.getUserChannel,
+  addMember:admin.addMember,
+  removeMember:admin.removeMember,
+  addSubject:admin.addSubject,
   addTag:admin.addTag,
   removeTag:admin.removeTag,
-  editSubject:admin.editSubject
+  editSubject:admin.editSubject,
+  editChannel:channel.editChannel,
+  deleteChannel:channel.deleteChannel
 };
